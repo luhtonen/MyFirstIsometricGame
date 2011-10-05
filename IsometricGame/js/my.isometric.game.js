91fc0ec3d4843308621cc337230d40196eaedcb6
Eduard Luhtonen <luhtonen@gmail.com>
2011-10-05 14:16:16 +0300

    creating blue gradient

--- IsometricGame/js/my.isometric.game.js
+++ IsometricGame/js/my.isometric.game.js
@@ -8,8 +8,12 @@ $(function() {
 	
 	var ctx = canvas.getContext('2d');
 	
-	// Fill the screen with a black background
-	ctx.fillStyle = '#000000';
+	// Fill the screen with a blue gradient
+	var grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+	grd.addColorStop(0, "#ceefff");
+	grd.addColorStop(1, "#52bcff");
+	
+	ctx.fillStyle = grd;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	
 	var phrase = "Click or tap the screen to start the game";
